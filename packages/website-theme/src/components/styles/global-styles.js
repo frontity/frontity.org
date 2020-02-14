db5fd73f87348404175c06ff89fdcfb6054c13e5
SantosGuillamot <santosguillamot@gmail.com>
2020-02-14 13:44:47 +0100

    Add common styles

--- packages/website-theme/src/components/styles/global-styles.js
+++ packages/website-theme/src/components/styles/global-styles.js
@@ -60,6 +60,11 @@ const documentSetup = colors => css`
   body {
     background: ${colors.wall};
     font-family: "IBMPlexSans";
+    color: ${colors.primary[80]};
+    max-width: 1080px;
+    margin: auto;
+    font-size: 16px;
+    line-height: 24px;
   }
   h1,
   h2,
@@ -68,9 +73,39 @@ const documentSetup = colors => css`
   h5,
   h6 {
     font-family: "Poppins";
+    color: ${colors.primary[100]};
   }
 `;
 
-const globalStyle = colors => css([cssReset, documentSetup(colors)]);
+const elementBase = colors => css`
+  h1 {
+    font-size: 48px;
+    line-height: 72px;
+    strong {
+      color: ${colors.frontity};
+    }
+  }
+  h2 {
+    font-size: 32px;
+    line-height: 40px;
+  }
+  h3 {
+    font-size: 24px;
+    line-height: 32px;
+  }
+  h4 {
+    font-size: 20px;
+    line-height: 24px;
+  }
+  h5 {
+    font-size: 14px;
+    line-height: 21px;
+    color: ${colors.primary[40]};
+    text-transform: uppercase;
+  }
+`;
+
+const globalStyle = colors =>
+  css([cssReset, documentSetup(colors), elementBase(colors)]);
 
 export default globalStyle;
