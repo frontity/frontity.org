a8a8075f8c3cb219e31280bd2df2873e5efc5783
SantosGuillamot <santosguillamot@gmail.com>
2020-02-14 11:46:38 +0100

    Add colors to theme settings

--- packages/website-theme/src/index.js
+++ packages/website-theme/src/index.js
@@ -6,7 +6,25 @@ const websiteTheme = {
     theme: Theme
   },
   state: {
-    theme: {}
+    theme: {
+      colors: {
+        frontity: "#1f38c5",
+        primary: {
+          100: rgba(15, 28, 100, 1),
+          80: rgba(15, 28, 100, 0.8),
+          60: rgba(15, 28, 100, 0.6),
+          40: rgba(15, 28, 100, 0.4)
+        },
+        voidBlu: "#0c112b",
+        grass: "#82c315",
+        wall: "#f6f6f9",
+        purple: "#1f38c5",
+        orange: "#f4c053",
+        red: "#f76d64",
+        turqoise: "#6ac8c9",
+        lightGreen: "#1f38c5"
+      }
+    }
   },
   actions: {
     theme: {}
