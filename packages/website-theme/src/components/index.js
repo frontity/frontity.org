797324a024b21ed24f47d63631562d4f65e6dccc
SantosGuillamot <santosguillamot@gmail.com>
2020-02-18 09:35:54 +0100

    Add Gutenberg css

--- packages/website-theme/src/components/index.js
+++ packages/website-theme/src/components/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Page from "./page";
-import { connect, Global } from "frontity";
+import { connect, Global, css } from "frontity";
 import globalStyles from "./styles/global-styles";
 import FontFace from "./styles/font-face";
+import gutenbergStyle from "./styles/gutenberg/style.css";
+import gutenbergTheme from "./styles/gutenberg/theme.css";
 
 const Theme = ({ state }) => {
   return (
     <>
       <Global styles={globalStyles(state.theme.colors)} />
+      <Global styles={css(gutenbergStyle)} />
+      <Global styles={css(gutenbergTheme)} />
       <FontFace />
       <Page />
     </>
