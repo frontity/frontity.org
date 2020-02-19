000da0d7f12cf3971fbd5a59d7d3bd49db2048d5
Mario <santosguillamot@gmail.com>
2020-02-19 18:34:53 +0100

    Update theme.colors settings

--- packages/website-theme/src/index.js
+++ packages/website-theme/src/index.js
@@ -9,20 +9,15 @@ const websiteTheme = {
     theme: {
       colors: {
         frontity: "#1f38c5",
-        primary: {
-          100: "rgba(15, 28, 100, 1)",
-          80: "rgba(15, 28, 100, 0.8)",
-          60: "rgba(15, 28, 100, 0.6)",
-          40: "rgba(15, 28, 100, 0.4)"
-        },
-        voidBlu: "#0c112b",
+        primary: "#0f1c64",
+        voidblu: "#0c112b",
         grass: "#82c315",
         wall: "#f6f6f9",
         purple: "#1f38c5",
         orange: "#f4c053",
         red: "#f76d64",
         turqoise: "#6ac8c9",
-        lightGreen: "#1f38c5"
+        lightgreen: "#8ACB88"
       }
     }
   },
