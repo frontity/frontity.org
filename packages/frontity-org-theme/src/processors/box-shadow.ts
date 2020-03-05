ebe679df8fc5908b697aa159aca67355916a4222
Uche Jude <iamuchejude@gmail.com>
2020-03-05 10:28:40 +0100

    Update css property and value for box-shadow

--- packages/frontity-org-theme/src/processors/box-shadow.ts
+++ packages/frontity-org-theme/src/processors/box-shadow.ts
@@ -5,14 +5,15 @@ const boxShadowProcessor = {
   test: node =>
     node.type === "element" &&
     node.props.className &&
-    node.props.className.split(" ").includes("has-shadow"),
+    node.props.className.split(" ").includes("has-box-shadow"),
   processor: ({ node }) => ({
     ...node,
     props: {
       ...node.props,
       css: css`
         ${node.props.css}
-        border: solid 1px rgba(15,28,100,0.12);
+        box-shadow: 0 4px 14px 0 rgba(31,56,197,0.09),
+                    0 2px 4px 0 rgba(31,56,197,0.12);
       `
     }
   })
