b48b7d8f5e5c24ad61a208cc58c2c32fb3048fd7
Uche Jude <iamuchejude@gmail.com>
2020-03-18 20:43:24 +0100

    Update global styles

--- packages/frontity-org-theme/src/components/styles/global-styles.tsx
+++ packages/frontity-org-theme/src/components/styles/global-styles.tsx
@@ -116,12 +116,12 @@ const elementBase = (colors: FrontityOrg["state"]["theme"]["colors"]) => css`
     list-style-type: "→ ";
 
     & > li {
-      margin: 1.1rem 0;
-      line-height: 1.3;
+      margin: 1.125rem 0;
+      line-height: 1.5rem;
 
       &::before {
         line-height: 0;
-        margin-right: 0.5rem;
+        margin: auto 0.5rem auto 0;
       }
     }
   }
