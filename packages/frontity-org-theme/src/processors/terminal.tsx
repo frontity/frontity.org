1e709ea8e6ca8a788821b59ee345e0f272da76dd
Michal Czaplinski <mmczaplinski@gmail.com>
2020-03-24 13:18:52 -0500

    Fix the terminal scrollbars

--- packages/frontity-org-theme/src/processors/terminal.tsx
+++ packages/frontity-org-theme/src/processors/terminal.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import FrontityOrg from "../../types";
 import { addAlpha } from "../utils";
 
+const TOP_HEIGHT = 24;
+
 const Dot = styled("span")<{ colors: { [key: string]: string } }>`
   display: inline-block;
   height: 8px;
@@ -19,6 +21,9 @@ const Top = ({ colors }) => (
   <div
     css={css`
       height: 24px;
+      background: ${colors.voidblu};
+      border-top-left-radius: inherit;
+      border-top-right-radius: inherit;
       border-bottom: 1px solid ${addAlpha(colors.white, 0.07)};
     `}
   >
@@ -50,7 +55,7 @@ export const terminal: Processor<React.HTMLProps<HTMLElement>, FrontityOrg> = {
       node.props.css = css`
         position: absolute;
         margin-left: 15px;
-        margin-top: 10px;
+        margin-top: ${TOP_HEIGHT + 10}px;
       `;
 
       // This class is used by the CSS for Prism.js syntax highlighter
@@ -60,6 +65,8 @@ export const terminal: Processor<React.HTMLProps<HTMLElement>, FrontityOrg> = {
 
     node.props.css = css`
       ${node.props.css}
+      display: flex;
+      flex-flow: column nowrap;
       font-family: Courier, monospace;
       font-size: 0.78rem;
       line-height: 1.65;
@@ -71,11 +78,34 @@ export const terminal: Processor<React.HTMLProps<HTMLElement>, FrontityOrg> = {
       box-shadow: 0 2px 12px 0 rgba(12, 17, 43, 0.4),
         0 1px 4px 0 rgba(12, 17, 43, 0.39);
       border-radius: 8px;
-      overflow: auto;
+      overflow: unset;
       max-width: 100%;
 
       .wp-block-group__inner-container {
-        margin-top: 12px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        overflow: auto;
+        height: 100%; /* need to specify height for overflow to work */
+
+        /* width */
+        &::-webkit-scrollbar {
+          width: 6px;
+        }
+        /* Track */
+        &::-webkit-scrollbar-track {
+          background: rgba(100, 100, 100, 0.5);
+          border-radius: 5px;
+          margin-bottom: 4px;
+        }
+        /* Handle */
+        &::-webkit-scrollbar-thumb {
+          background: #888;
+          border-radius: 5px;
+        }
+        /* Handle on hover */
+        &::-webkit-scrollbar-thumb:hover {
+          background: #555;
+        }
       }
 
       ol {
