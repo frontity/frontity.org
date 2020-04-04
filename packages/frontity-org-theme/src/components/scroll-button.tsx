e8021b45b0832f787b6388b5a7bfadd93296bedb
SantosGuillamot <santosguillamot@gmail.com>
2020-04-04 11:12:11 +0200

    Reorganize scroll button component

--- packages/frontity-org-theme/src/components/scroll-button.tsx
+++ packages/frontity-org-theme/src/components/scroll-button.tsx
@@ -1,4 +1,4 @@
-import { connect, styled } from "frontity";
+import { connect, css,styled } from "frontity";
 import { Connect } from "frontity/types";
 import React from "react";
 
@@ -6,37 +6,41 @@ import FrontityOrg from "../../types";
 
 const ScrollButton: React.FC<Connect<FrontityOrg, any>> = ({ state }) => {
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    if (typeof window !== "undefined") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
   };
 
-  const Button = styled.div`
-    @media only screen and (min-width: 768px) {
-      display: none;
-    }
-    position: fixed;
-    bottom: 16px;
-    right: 16px;
-    background-color: ${state.theme.colors.frontity};
-    height: 56px;
-    width: 56px;
-    border-radius: 50%;
-    cursor: pointer;
-    text-align: center;
-    box-shadow: 0 4px 8px 0 rgba(12, 17, 43, 0.12),
-      0 1px 4px 0 rgba(12, 17, 43, 0.16);
-    color: ${state.theme.colors.white};
-    line-height: 56px;
-    font-size: 24px;
-    font-weight: bold;
-  `;
-  return typeof window !== "undefined" ? (
-    <Button onClick={scrollToTop}>
-      <p>↑</p>
+  return (
+    <Button
+      css={css`
+        background-color: ${state.theme.colors.frontity};
+        color: ${state.theme.colors.white};
+      `}
+      onClick={scrollToTop}
+    >
+      ↑
     </Button>
-  ) : null;
+  );
 };
 
 export default connect(ScrollButton);
+
+const Button = styled.div`
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+  height: 56px;
+  width: 56px;
+  border-radius: 50%;
+  cursor: pointer;
+  text-align: center;
+  box-shadow: 0 4px 8px 0 rgba(12, 17, 43, 0.12),
+    0 1px 4px 0 rgba(12, 17, 43, 0.16);
+  line-height: 56px;
+  font-size: 24px;
+  font-weight: bold;
+`;
