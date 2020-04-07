e8115be088d581bea1c992e475ab7b87978690ad
Michal Czaplinski <mmczaplinski@gmail.com>
2020-04-07 13:05:58 -0500

    Add the flowing button

--- packages/frontity-org-theme/src/components/frontity-flow-button.tsx
+++ packages/frontity-org-theme/src/components/frontity-flow-button.tsx
@@ -5,7 +5,10 @@ import React from "react";
 import FrontityOrg from "../../types";
 import { addAlpha } from "../utils";
 
-const Button = styled.button`
+const StyledButton = styled.button`
+  z-index: 20;
+  position: relative;
+
   margin: 6px;
   height: 52px;
   width: 158px;
@@ -22,57 +25,45 @@ const Button = styled.button`
     outline: none;
   }
 
+  background-color: transparent;
+  color: ${({ colors }) => colors.primary};
+
   cursor: pointer;
 
-  transition-property: background-color, box-shadow;
-  transition-duration: 200ms;
-  transition-timing-function: ease-in-out;
+  transition: background-color 200ms ease-in-out, color 300ms ease-in-out 300ms;
 
-  ${({ colors, active, number }) =>
+  &:before {
+    content: '${({ number }) => number}';
+    display: inline-block;
+    padding: 2px;
+    vertical-align: text-bottom;
+    width: 20px;
+    height: 20px;
+    margin-right: 12px;
+    font-size: 12px;
+    border-radius: 50%;
+    color: white;
+    background-color: ${addAlpha(`#ffffff`, 0.15)};
+
+    transition: all 400ms ease-in-out 200ms;
+  };
+
+  ${({ active, colors }) =>
     active
       ? css`
-          background-color: ${colors.frontity};
           color: white;
-          box-shadow: 0 4px 8px 0 rgba(12, 17, 43, 0.12),
-            0 1px 4px 0 rgba(12, 17, 43, 0.16);
-
-            &:before {
-              content: '${number}';
-              display: inline-block;
-              padding: 2px;
-              vertical-align: text-bottom;
-              width: 20px;
-              height: 20px;
-              margin-right: 12px;
-              font-size: 12px;
-              border-radius: 50%;
-              color: white;
-              background-color: ${addAlpha(`#ffffff`, 0.15)};
-            }
         `
       : css`
-          background-color: transparent;
-          color: ${colors.primary};
-
           &:hover {
             filter: opacity(0.9);
             background-color: ${addAlpha(colors.frontity, 0.08)};
           }
 
           &:before {
-              content: '${number}';
-              display: inline-block;
-              vertical-align: text-bottom;
-              padding: 2px;
-              width: 20px;
-              height: 20px;
-              margin-right: 12px;
-              font-size: 12px;
-              border-radius: 50%;
-              color: ${colors.grass};
-              background-color: ${addAlpha(colors.grass, 0.15)};
-            };
-        `};
+            color: ${colors.grass};
+            background-color: ${addAlpha(colors.grass, 0.15)};
+          }
+        `}
 `;
 
 const FlowButton: React.FC<Connect<
@@ -89,18 +80,18 @@ const FlowButton: React.FC<Connect<
   fills,
 }) => {
   const { tabNumber: activeTab, colors } = state.theme;
-  const table = { 1: 2, 2: 3, 3: 4, 4: 3 };
+  const transitionsTable = { 1: 2, 2: 3, 3: 4, 4: 3 };
 
   return (
-    <Button
+    <StyledButton
       number={tabNumber}
       colors={colors}
       active={activeTab === tabNumber}
-      following={tabNumber === table[activeTab]}
+      following={tabNumber === transitionsTable[activeTab]}
       onClick={() => actions.theme.setTabNumber({ tabNumber })}
     >
       {text}
-    </Button>
+    </StyledButton>
   );
 };
 
--- packages/frontity-org-theme/src/processors/frontity-flow-buttons.tsx
+++ packages/frontity-org-theme/src/processors/frontity-flow-buttons.tsx
@@ -1,8 +1,55 @@
-import { Processor } from "@frontity/html2react/types";
-import { css } from "frontity";
+import { Node, Processor } from "@frontity/html2react/types";
+import { css, styled } from "frontity";
 import React from "react";
 
 import FrontityOrg from "../../types";
+
+const SlidingButton = styled.div`
+  z-index: 10;
+  position: absolute;
+  left: 0;
+  top: 0;
+
+  margin: 6px;
+  height: 52px;
+  width: 158px;
+  padding: 12px 16px;
+  border-radius: 12px;
+  border: 0;
+
+  background-color: ${({ frontity }) => frontity};
+  color: white;
+  box-shadow: 0 4px 8px 0 rgba(12, 17, 43, 0.12),
+    0 1px 4px 0 rgba(12, 17, 43, 0.16);
+
+  transition: transform 600ms ease-in-out;
+
+  ${({ activeTab }) =>
+    activeTab === 1
+      ? css`
+          transform: translateX(0);
+        `
+      : activeTab === 2
+      ? css`
+          transform: translateX(100%);
+        `
+      : activeTab === 3
+      ? css`
+          transform: translateX(200%);
+        `
+      : css`
+          transform: translateX(300%);
+        `};
+
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 0;
+  line-height: 16px;
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 export const flowButtons: Processor<
   React.HTMLProps<HTMLElement>,
@@ -12,16 +59,40 @@ export const flowButtons: Processor<
   test: ({ node }) =>
     node.type === "element" &&
     node.props?.className?.split(" ").includes("frontity-flow-buttons"),
-  processor: ({ node }) => {
+  processor: ({ node, state }) => {
     if (node.type !== "element") return node;
 
     node.props.css = css`
       ${node.props.css};
 
+      z-index: 10;
+
+      position: relative;
+      width: 640px;
+
+      margin: 0 auto;
+
+      display: flex;
+      flex-flow: row nowrap;
+      justify-items: center;
+
       @media screen and (max-width: 700px) {
         display: none;
       }
     `;
+
+    const slidingButton: Node<
+      React.HTMLProps<HTMLElement> & { activeTab: number; frontity: string }
+    > = {
+      type: "element",
+      component: SlidingButton,
+      props: {
+        activeTab: state.theme.tabNumber,
+        frontity: state.theme.colors.frontity,
+      },
+    };
+
+    node.children.push(slidingButton);
 
     return node;
   },
