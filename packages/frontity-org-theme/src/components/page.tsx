f9616a7b6d2972e474079819bdeff51b820413a2
Michal Czaplinski <mmczaplinski@gmail.com>
2020-04-16 13:08:31 -0500

    Bring back the triangles background

--- packages/frontity-org-theme/src/components/page.tsx
+++ packages/frontity-org-theme/src/components/page.tsx
@@ -3,14 +3,17 @@ import { Connect } from "frontity/types";
 import React from "react";
 
 import FrontityOrg from "../../types";
+import BackgroundWithTriangles from "../components/background-with-triangles";
 
 const Page: React.FC<Connect<FrontityOrg>> = ({ state, libraries }) => {
   const data = state.source.get(state.router.link);
   const Html2React = libraries.html2react.Component;
   const page = state.source.page[data.id];
+  const shouldHaveBackground = "background_triangles" in page?.acf;
 
   return (
     <>
+      {shouldHaveBackground && <BackgroundWithTriangles height="150vh" />}
       <Html2React html={page.content.rendered} />
     </>
   );
