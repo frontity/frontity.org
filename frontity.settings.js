d25a8cadef46ca4427e6d4399c6f68eb8001ba2d
SantosGuillamot <santosguillamot@gmail.com>
2020-06-02 08:19:58 +0200

    Point to frontity.org again

--- frontity.settings.js
+++ frontity.settings.js
@@ -16,7 +16,7 @@ const settings = [
         name: "@frontity/wp-source",
         state: {
           source: {
-            api: "https://lighthouse-frontity-org.pantheonsite.io/wp-json",
+            api: "https://frontity.org/wp-json",
             postTypes: [
               {
                 type: "/blog/wp_template_part",
@@ -103,7 +103,7 @@ const settings = [
         name: "@frontity/wp-source",
         state: {
           source: {
-            api: "https://lighthouse-frontity-org.pantheonsite.io/wp-json",
+            api: "https://frontity.org/wp-json",
             homepage: "/homepage/",
             postsPage: "/blog",
             categoryBase: "/blog/category",
