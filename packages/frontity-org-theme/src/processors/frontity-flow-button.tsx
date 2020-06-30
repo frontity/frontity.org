4cb44846d3e9f1e808d918345ccc082549807e14
SantosGuillamot <santosguillamot@gmail.com>
2020-06-30 10:23:11 +0200

    Fix frontity flow buttons

--- packages/frontity-org-theme/src/processors/frontity-flow-button.tsx
+++ packages/frontity-org-theme/src/processors/frontity-flow-button.tsx
@@ -32,7 +32,7 @@ export const flowButton: Processor<
       component: FlowButton,
       props: {
         tabNumber: parseInt(tabNumber),
-        text: (node as any).children[1]?.content,
+        text: (node as any).children[0].children[0]?.content,
       },
     };
   },
