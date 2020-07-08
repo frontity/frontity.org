52af7d44f134f524749750e7372a017506213877
David Arenas <darensr@gmail.com>
2020-07-08 13:40:10 +0200

    Fix dropdown processor types

--- packages/frontity-org-theme/src/processors/dropdown.tsx
+++ packages/frontity-org-theme/src/processors/dropdown.tsx
@@ -1,6 +1,16 @@
+import { Element, Processor } from "@frontity/html2react/types";
+
 import Dropdown from "../components/dropdown";
 
-export const dropdown = {
+interface DropdownItem extends Element {
+  children: [DropdownInnerContainer];
+}
+
+interface DropdownInnerContainer extends Element {
+  children: [Element, Element];
+}
+
+export const dropdown: Processor<DropdownItem> = {
   name: "dropdown",
   test: ({ node }) =>
     node.type === "element" &&
@@ -10,15 +20,15 @@ export const dropdown = {
     // Get children from the inner component.
     const [dropdownButton, dropdownContent] = node.children[0].children;
 
-    /**
-     * Set them as "children" (we cannot pass them as other props because)
-     * they wouldn't be rendered. Also, other processors wouldn't run.
-     */
-    node.children = [dropdownButton, dropdownContent];
-
-    // Change node component by Dropdown.
-    node.component = Dropdown;
-
-    return node;
+    return {
+      ...node,
+      /**
+       * Set them as "children" (we cannot pass them as other props because)
+       * they wouldn't be rendered. Also, other processors wouldn't run.
+       */
+      children: [dropdownButton, dropdownContent],
+      // Change node component by Dropdown.
+      component: Dropdown,
+    };
   },
 };
