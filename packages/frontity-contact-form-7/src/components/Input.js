15b82138dd4eaebfd606c00c6a18d953fa7eb4f4
SantosGuillamot <santosguillamot@gmail.com>
2020-09-22 18:18:52 +0200

    Add support for checkboxes

--- packages/frontity-contact-form-7/src/components/Input.js
+++ packages/frontity-contact-form-7/src/components/Input.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react';
-import FormIdContext from "../context/FormIdContext";
 import { connect } from "frontity";
+import React, { useContext } from "react";
 
+import FormIdContext from "../context/FormIdContext";
 /**
  * Input Component.
  *
@@ -10,43 +10,56 @@ import { connect } from "frontity";
  * @param {Object} inputProps Input props.
  * @return {*}
  */
-const Input = ( { state, actions, inputProps } ) => {
-
-	// Context is used so that we can pass the form id to different components.
-	const id          = useContext( FormIdContext );
-	const inputName   = inputProps.name;
-	const placeholder = inputProps.placeholder;
-
-	if ( 'undefined' === typeof ( state.cf7.forms[ id ].inputVals[ inputName ] ) ) {
-		actions.cf7.changeInputValue( { id, inputName, value: inputProps.value } );
-	}
-
-	/**
-	 * OnChange handler for input.
-	 *
-	 * @param {Object} event Event.
-	 *
-	 * @return {void}
-	 */
-	const onChange = ( event ) => {
-
-		actions.cf7.changeInputValue( { id, inputName, value: event.target.value } );
-
-	};
-
-	return (
-		<input
-			name={ inputProps.name }
-			className={ inputProps.className }
-			aria-invalid={ inputProps.ariaInvalid }
-			aria-required={ inputProps.ariaRequired }
-			size={ inputProps.size }
-			type={ inputProps.type }
-			value={ state.cf7.forms[ id ].inputVals[ inputName ] }
-			onChange={ onChange }
-			placeholder={ placeholder }
-		/>
-	);
+const Input = ({ state, actions, inputProps }) => {
+  // Context is used so that we can pass the form id to different components.
+  const id = useContext(FormIdContext);
+  const inputName = inputProps.name;
+  const placeholder = inputProps.placeholder;
+  if ("undefined" === typeof state.cf7.forms[id].inputVals[inputName]) {
+    const value =
+      inputProps.type === "checkbox" ? inputProps.checked : inputProps.value;
+    actions.cf7.changeInputValue({ id, inputName, value });
+  }
+  /**
+   * OnChange handler for input.
+   *
+   * @param {Object} event Event.
+   *
+   * @return {void}
+   */
+  const onChange = (event) => {
+    let value =
+      inputProps.type === "checkbox"
+        ? !event.target.checked
+          ? ""
+          : event.target.checked
+        : event.target.value;
+    actions.cf7.changeInputValue({ id, inputName, value });
+  };
+  return inputProps.type === "checkbox" ? (
+    <input
+      name={inputProps.name}
+      className={inputProps.className}
+      aria-invalid={inputProps.ariaInvalid}
+      aria-required={inputProps.ariaRequired}
+      size={inputProps.size}
+      type={inputProps.type}
+      checked={state.cf7.forms[id].inputVals[inputName]}
+      onChange={onChange}
+      placeholder={inputProps.placeholder}
+    />
+  ) : (
+    <input
+      name={inputProps.name}
+      className={inputProps.className}
+      aria-invalid={inputProps.ariaInvalid}
+      aria-required={inputProps.ariaRequired}
+      size={inputProps.size}
+      type={inputProps.type}
+      value={state.cf7.forms[id].inputVals[inputName]}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
 };
-
-export default connect( Input );
+export default connect(Input);
--- packages/frontity-contact-form-7/src/processors/cf7Inputs.js
+++ packages/frontity-contact-form-7/src/processors/cf7Inputs.js
@@ -1,35 +1,52 @@
-import Input from '../components/Input';
-
+import Input from "../components/Input";
 const cf7Inputs = {
-
-	name: "cf7Inputs",
-	test: ({ node }) => node.component === "input" && /wpcf7-form-control/.test( node.props.className ),
-	processor: ({ node }) => {
-
-		const ariaInvalid  = ( 'undefined' === typeof ( node.props[ 'aria-invalid' ] ) ) ? '' : node.props[ 'aria-invalid' ];
-		const ariaRequired = ( 'undefined' === typeof ( node.props[ 'aria-required' ] ) ) ? '' : node.props[ 'aria-required' ];
-		const className    = ( 'undefined' === typeof ( node.props.className ) ) ? '' : node.props.className;
-		const name         = ( 'undefined' === typeof ( node.props.name ) ) ? '' : node.props.name;
-		const size         = ( 'undefined' === typeof ( node.props.size ) ) ? '' : node.props.size;
-		const type         = ( 'undefined' === typeof ( node.props.type ) ) ? '' : node.props.type;
-		const value        = ( 'undefined' === typeof ( node.props.value ) ) ? '' : node.props.value;
-		const placeholder  = ( 'undefined' === typeof ( node.props.placeholder ) ) ? '' : node.props.placeholder;
-
-		node.props.inputProps = {
-			ariaInvalid: ariaInvalid,
-			ariaRequired: ariaRequired,
-			className: className,
-			name: name,
-			size: size,
-			type: type,
-			value: value,
-			placeholder: placeholder
-		};
-
-		node.component = Input;
-		return node;
-	}
-
+  name: "cf7Inputs",
+  test: ({ node }) => {
+    return (
+      node.component === "input" &&
+      (/wpcf7-form-control/.test(node.props.className) ||
+        (node.props.type === "checkbox" &&
+          node.parent.children[1].props.className
+            .split(" ")
+            .includes("wpcf7-list-item-label")))
+    );
+  },
+  processor: ({ node }) => {
+    console.log(node);
+    const ariaInvalid =
+      "undefined" === typeof node.props["aria-invalid"]
+        ? ""
+        : node.props["aria-invalid"];
+    const ariaRequired =
+      "undefined" === typeof node.props["aria-required"]
+        ? ""
+        : node.props["aria-required"];
+    const className =
+      "undefined" === typeof node.props.className ? "" : node.props.className;
+    const name = "undefined" === typeof node.props.name ? "" : node.props.name;
+    const size = "undefined" === typeof node.props.size ? "" : node.props.size;
+    const type = "undefined" === typeof node.props.type ? "" : node.props.type;
+    const value =
+      "undefined" === typeof node.props.value ? "" : node.props.value;
+    const checked =
+      "undefined" === typeof node.props.checked ? "" : node.props.checked;
+    const placeholder =
+      "undefined" === typeof node.props.placeholder
+        ? ""
+        : node.props.placeholder;
+    node.props.inputProps = {
+      ariaInvalid: ariaInvalid,
+      ariaRequired: ariaRequired,
+      className: className,
+      name: name,
+      size: size,
+      type: type,
+      value: value,
+      checked: checked,
+      placeholder: placeholder,
+    };
+    node.component = Input;
+    return node;
+  },
 };
-
 export default cf7Inputs;
