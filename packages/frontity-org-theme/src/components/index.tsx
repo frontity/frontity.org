b2a2c4623b6054777c36821bcbc52727089fce38
SantosGuillamot <santosguillamot@gmail.com>
2020-09-22 11:27:53 +0200

    Add styles to CF7 forms

--- packages/frontity-org-theme/src/components/index.tsx
+++ packages/frontity-org-theme/src/components/index.tsx
@@ -8,6 +8,7 @@ import { FixedHeader, Header } from "./headers";
 import Page from "./page";
 import PageError from "./page-error";
 import ScrollButton from "./scroll-button";
+import cf7Styles from "./styles/cf7";
 import FontFace from "./styles/font-face";
 import globalStyles from "./styles/global-styles";
 import gutenbergStyle from "./styles/gutenberg/style.css";
@@ -23,6 +24,7 @@ const Theme: React.FC<Connect<FrontityOrg>> = ({ state }) => {
       <Global styles={css(gutenbergStyle)} />
       <Global styles={css(gutenbergTheme)} />
       <Global styles={globalStyles(state.theme.colors)} />
+      <Global styles={cf7Styles(state.theme.colors)} />
 
       <FontFace />
       <Head>
