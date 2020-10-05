98392846952413548d9701c510987c609f5a0600
Michal Czaplinski <mmczaplinski@gmail.com>
2020-10-05 17:35:11 -0500

    Add a processor for prism.js to process the styles on server and client

--- packages/twentytwenty-theme/src/index.js
+++ packages/twentytwenty-theme/src/index.js
@@ -1,8 +1,7 @@
-import "./prism";
-
 import image from "@frontity/html2react/processors/image";
 
 import Theme from "./components";
+import { prismProcessor } from "./prism-processor";
 
 const twentyTwentyTheme = {
   name: "@frontity/twentytwenty-theme",
@@ -82,7 +81,7 @@ const twentyTwentyTheme = {
        * Add a processor to `html2react` so it processes the `<img>` tags
        * inside the content HTML. You can add your own processors too
        */
-      processors: [image],
+      processors: [image, prismProcessor],
     },
   },
 };
