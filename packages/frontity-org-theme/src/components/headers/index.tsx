bebc53d385832dbe7331bb2fc5584152974b1278
Luis Herranz <luisherranz@gmail.com>
2021-07-01 17:02:08 +0200

    Improve types in header

--- packages/frontity-org-theme/src/components/headers/index.tsx
+++ packages/frontity-org-theme/src/components/headers/index.tsx
@@ -1,74 +1,80 @@
-import { connect, Head } from "frontity";
+import { connect, Head, useConnect } from "frontity";
 import { Connect } from "frontity/types";
+import { isPostType } from "@frontity/source";
 import React from "react";
 import { useInView } from "react-intersection-observer";
-
-import FrontityOrg from "../../../types";
+import FrontityOrg, { Packages } from "../../../types";
 import HeaderButton from "./header-button";
 import { fixedHeaderStyles, headerStyles } from "./header-styles";
 
-export const Header = connect<React.FC<Connect<FrontityOrg>>>(
-  ({ state, actions, libraries }) => {
-    // Get the header template.
-    const data = state.source.get("/blog/wp_template_part/header-web/");
-    const header = state.source["wp_template_part"][data.id];
+export const Header = connect(() => {
+  const { state, actions, libraries } = useConnect<Packages>();
+  // Get the header template.
+  const data = state.source.get("/blog/wp_template_part/header-web/");
 
-    // Get the component that transform the template to React.
-    const Html2React = libraries.html2react.Component;
+  // Bail out if the data returned doesn't belong to a post type.
+  if (!isPostType(data)) return null;
 
-    // Store if the menu is open (for the mobile view).
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const header = state.source["wp_template_part"][data.id];
 
-    // Check if this bar is visible or not.
-    const [ref, isInView] = useInView();
+  // Get the component that transform the template to React.
+  const Html2React = libraries.html2react.Component;
 
-    React.useEffect(() => {
-      if (isInView) {
-        actions.theme.hideFixedHeader();
-      } else {
-        actions.theme.showFixedHeader();
-      }
-    }, [isInView]);
+  // Store if the menu is open (for the mobile view).
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-    // Generate styles.
-    const styles = headerStyles({ state, isMenuOpen });
+  // Check if this bar is visible or not.
+  const [ref, isInView] = useInView();
 
-    return (
-      <div ref={ref} css={styles}>
-        {/* Render the template */}
-        <Html2React html={header.content.rendered} />
-        {/* Render the menu button (only on mobile).*/}
-        <HeaderButton
-          color={state.theme.colors.frontity}
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          isMenuOpen={isMenuOpen}
-        />
-        {/* Make body unscrollabe when menu is open (only on mobile). */}
-        {isMenuOpen && (
-          <Head>
-            <style>{`body { overflow-y: hidden; }`}</style>
-          </Head>
-        )}
-      </div>
-    );
-  }
-);
+  React.useEffect(() => {
+    if (isInView) {
+      actions.theme.hideFixedHeader();
+    } else {
+      actions.theme.showFixedHeader();
+    }
+  }, [isInView]);
 
-export const FixedHeader = connect<React.FC<Connect<FrontityOrg>>>(
-  ({ state, libraries }) => {
-    // Get the header template.
-    const data = state.source.get("/blog/wp_template_part/fixed-header/");
-    const header = state.source["wp_template_part"][data.id];
+  // Generate styles.
+  const styles = headerStyles({ state, isMenuOpen });
 
-    // Get the component that transform the template to React.
-    const Html2React = libraries.html2react.Component;
-    const { isFixedHeaderVisible } = state.theme;
+  return (
+    <div ref={ref} css={styles}>
+      {/* Render the template */}
+      <Html2React html={header.content.rendered} />
+      {/* Render the menu button (only on mobile).*/}
+      <HeaderButton
+        color={state.theme.colors.frontity}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        isMenuOpen={isMenuOpen}
+      />
+      {/* Make body unscrollabe when menu is open (only on mobile). */}
+      {isMenuOpen && (
+        <Head>
+          <style>{`body { overflow-y: hidden; }`}</style>
+        </Head>
+      )}
+    </div>
+  );
+});
 
-    return (
-      <div css={fixedHeaderStyles({ state, isFixedHeaderVisible })}>
-        {/* Render the template */}
-        <Html2React html={header.content.rendered} />
-      </div>
-    );
-  }
-);
+export const FixedHeader = connect(() => {
+  const { state, libraries } = useConnect<Packages>();
+  // Get the header template.
+  const data = state.source.get("/blog/wp_template_part/fixed-header/");
+
+  // Bail out if the data returned doesn't belong to a post type.
+  if (!isPostType(data)) return null;
+
+  const header = state.source["wp_template_part"][data.id];
+
+  // Get the component that transform the template to React.
+  const Html2React = libraries.html2react.Component;
+  const { isFixedHeaderVisible } = state.theme;
+
+  return (
+    <div css={fixedHeaderStyles({ state, isFixedHeaderVisible })}>
+      {/* Render the template */}
+      <Html2React html={header.content.rendered} />
+    </div>
+  );
+});
